00cee1827eda94601013494ccc312e277b122774
GiovannaDP <giovannadanellipau@gmail.com>
2023-01-17 22:27:27 -0300

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Giovanna Danelli Pau on 17/01/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
